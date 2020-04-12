79cc1bcc50f7ac173ddca35188f27dcd5b8f3935
K0val0K <60182247+K0val0K@users.noreply.github.com>
2020-04-12 16:02:49 +0300

    Add files via upload

---
+++ 2nd_sem/Рекурсия/Вариант 10/Вариант 10/Вариант 10.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+double prod(double);
+double recprod(double);
+
+int main() {
+	double n;
+	cout << "Input n(n>=2): ";
+	cin >> n;
+	//cout << "NOT reccurent product: " << prod(n) << endl;
+	cout << "reccurent product: " << recprod(n) << endl;
+	return 0;
+}
+double prod(double n) {
+	for (n; n >= 1; n--) {
+
+	}
+ }
+
+double recprod(double n) {
+	if (n == 1) {
+		return 2;
+	}
+	if (static_cast<int>(n) % 2 == 0) {
+		return (n / (n + 1)) * recprod(n - 1);
+	}
+	if (static_cast<int>(n) % 2 != 0) {
+		return ((n + 1) / n) * recprod(n - 1);
+	}
+}
